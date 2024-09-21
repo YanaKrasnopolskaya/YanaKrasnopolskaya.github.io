e6d71746bd2a29acc8b867a772c0873fb10c2415
Yana_Krasnopolskaya <krasnopolskaya.yana331@gmail.com>
2024-09-21 15:24:40 +0700

    add form and event

--- src/script.js
+++ src/script.js
@@ -1,14 +1,76 @@
-const menuInputElement = document.getElementById('menu');
-const menuBoxElement = document.querySelector('.menu-box');
-const menuCloseElement = document.querySelector('.menu-box__close');
+const menuInputEl = document.getElementById('menu');
+const menuBoxEl = document.querySelector('.menu-box');
+const menuCloseEl = document.querySelector('.menu-box__close');
+const formEmailEl = document.querySelector('.content__form');
+const inputEmailEl = document.querySelector('.content__form_input');
 
-menuInputElement.addEventListener('change', (e) => {
-    menuInputElement.checked = 'true';
-    menuBoxElement.style.right = 0;
+// menu press tracking
+menuInputEl.addEventListener('change', (e) => {
+    menuInputEl.checked = 'true';
+    menuBoxEl.style.right = 0;
 });
 
-menuCloseElement.addEventListener('click', (e) => {
+// menu close press tracking
+menuCloseEl.addEventListener('click', (e) => {
     e.preventDefault();
-    menuInputElement.checked = 'false';
-    menuBoxElement.style.right = '-100%';
+    menuInputEl.checked = 'false';
+    menuBoxEl.style.right = '-100%';
+});
+
+// email validity check function
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// sending event
+inputEmailEl.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) {
+        if (!validateEmail(inputEmailEl.value) || !inputEmailEl.value) {
+            e.target.blur();
+
+            const errorEmail = '<span class="content__form_error-email">E-mail</span>';
+            const errorMessage = '<span class="content__form_error">текст ошибки</span>';
+
+            inputEmailEl.insertAdjacentHTML('beforebegin', errorEmail);
+            inputEmailEl.insertAdjacentHTML('afterend', errorMessage);
+            
+            inputEmailEl.value = '';
+            inputEmailEl.style.borderBottom = '4px solid rgb(232, 15, 59)';
+            return false;
+        } else {
+            formEmailEl.submit();
+        }
+    }
+});
+
+inputEmailEl.addEventListener('blur', (e) => {
+    inputEmailEl.style.borderBottom = '2px solid rgb(174, 173, 174)';
+});
+
+inputEmailEl.addEventListener('mouseover', (e) => {
+    inputEmailEl.style.borderBottom = '2px solid rgb(62, 41, 227)';
+});
+
+inputEmailEl.addEventListener('mouseout', (e) => {
+    inputEmailEl.style.borderBottom = '2px solid rgb(174, 173, 174)';
+});
+
+// tracking of new blocks for correct operation of the form
+const observer = new MutationObserver(() => {
+    const errorEl = document.querySelector('.content__form_error-email');
+    const errorMessageEl = document.querySelector('.content__form_error');
+
+    inputEmailEl.addEventListener('focus', (e) => {
+        inputEmailEl.style.borderBottom = '4px solid rgb(62, 41, 227)';
+        if (errorEl && errorMessageEl) {
+            errorEl.remove();
+            errorMessageEl.remove();
+        }
+    });
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
 });
